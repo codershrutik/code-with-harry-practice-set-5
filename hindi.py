19895e407d4153bc4a6c95aca4729a0e36a42246
codershrutik <72141758+codershrutik@users.noreply.github.com>
2022-09-11 17:07:12 +0530

    Add files via upload

---
+++ hindi.py
@@ -0,0 +1,6 @@
+a = {"namaste":"hello/welcome",
+     "kaise hain aap?":"how are you",
+     "nahi":"no",
+     "haan":"yes"}
+
+print(a)
---
+++ unique.py
@@ -0,0 +1,8 @@
+my_list = list(input("Enter the list: "))
+
+print("Original List : ",my_list)
+
+my_set = set(my_list)
+my_new_list = list(my_set)
+
+print("List of unique numbers : ",my_new_list)
